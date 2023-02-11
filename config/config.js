b4486d642eb905915a643011ffaefceb2e03673e
Juan Mrad <juansmrad@gmail.com>
2023-02-11 09:36:52 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "use_env_variable": "DB_URL"
   }
 }
